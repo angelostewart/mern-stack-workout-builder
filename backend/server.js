84cab2ebe917bf6fd917b5c4b63b43b69861725b
Angelo Stewart <angelostewart@protonmail.com>
2023-02-27 22:11:24 -0500

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts");
+const userRoutes = require("./routes/user");
+
+// express app
+const app = express();
+
+// middleware
+// looks for a body for any request
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.use("/api/workouts", workoutRoutes);
+app.use("/api/user", userRoutes);
+
+// connect to DB
+mongoose
+  .connect(process.env.MONG_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("connected to DB & listening on port 4000");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
